a3c8dadf07e3efe71dc69e61e0fcc02f3b564255
biggreyhairboy <biggreyhairboy@gmail.com>
2015-05-12 15:22:11 +0800

    get() to get every word of the whole file till the end!

--- ReadAndWriteFile/main.cpp
+++ ReadAndWriteFile/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+
 int main()
 {
+	//if (cin){
+	//	cout << "cin is ok";
+	//}
+	ifstream infile;
+	string str;
+	char AChar;
+	infile.open("abc.txt", ifstream::in);
+	while ((AChar = infile.get() != EOF)){
+		cout << AChar;
+		
+	}
+	
+	getchar();
 
+}
+static  istream& streamParser(istream&)
+{	
 }
